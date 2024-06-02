3720acd2c01ee7dc471bd385f09f4f8927efa63e
mpg-data <ing.padilla@outlook.com>
2024-06-02 00:40:34 +0000

    Resolver tarea 2 completa

---
+++ homework-2/hpo.py
@@ -0,0 +1,71 @@
+import os
+import pickle
+import click
+import mlflow
+import numpy as np
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+from hyperopt.pyll import scope
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+
+TRACKING_SERVER_HOST = "ec2-3-143-249-167.us-east-2.compute.amazonaws.com" # fill in with the public DNS of the EC2 instance
+mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:5000")
+mlflow.set_experiment("random-forest-hyperopt")
+#mlflow experiments restore --experiment-id
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+@click.option(
+    "--num_trials",
+    default=15,
+    help="The number of parameter evaluations for the optimizer to explore"
+)
+def run_optimization(data_path: str, num_trials: int):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    def objective(params):
+
+        with mlflow.start_run():
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = root_mean_squared_error(y_val, y_pred)
+            mlflow.log_metric("rmse", rmse)
+
+        return {'loss': rmse, 'status': STATUS_OK}
+
+    search_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
+        'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
+        'min_samples_split': scope.int(hp.quniform('min_samples_split', 2, 10, 1)),
+        'min_samples_leaf': scope.int(hp.quniform('min_samples_leaf', 1, 4, 1)),
+        'random_state': 42
+    }
+
+    rstate = np.random.default_rng(42)  # for reproducible results
+    fmin(
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=num_trials,
+        trials=Trials(),
+        rstate=rstate
+    )
+
+
+if __name__ == '__main__':
+    run_optimization()
